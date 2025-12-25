7839cc4c2f967d81ca67d838bc6f7b60b0d111ea
PUMIN-L <puminbg@gmail.com>
2025-12-25 12:49:07 +0700

    test cloudinary code

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -4,7 +4,6 @@ cloudinary.config({
     cloud_name: process.env.CLOUDINALY_NAME,
     api_key: process.env.CLOUDINALY_API_KEY,
     api_secret: process.env.CLOUDINALY_API_SECRET
-
 })
 
 module.exports = cloudinary
